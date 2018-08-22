8bfb1491fc00ef77ebd61bb1a0c581a7fefcdc67
Srija S <srijakumar7@gmail.com>
2018-08-22 00:05:02 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -20,7 +20,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    self.where("rating DESC LIMIT 1")
+    self.where("rating ORDER BY DESC LIMIT 1")
   end
 
   def popular_shows
