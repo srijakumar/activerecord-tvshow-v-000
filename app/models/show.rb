c6073a9ec7450fe72afa52211578b90820dc417d
Srija S <srijakumar7@gmail.com>
2018-08-22 00:11:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -27,8 +27,8 @@ class Show < ActiveRecord::Base
     self.where("rating > 5")
   end
 
-  def shows_by_alphabetical_order
-
+  def self.shows_by_alphabetical_order
+    self.order(:name)
   end
 
 end
